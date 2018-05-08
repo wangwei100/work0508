16961cc617917fd0b00661060161675754bbe241
canger <canger@demohot.com>
2018-05-08 21:15:44 +0800

    xxx

---
+++ src/main/java/com/demohot/homework/work0508/App.java
@@ -0,0 +1,13 @@
+package com.demohot.homework.work0508;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
